5b8514eabb5c227ec4811fd6e98f1bac15d8b1d3
SauravKanchan <sauravnk30@gmail.com>
2020-04-13 22:06:57 +0530

    Initialize chart component

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -1,152 +1,8 @@
-<style>
-  tr.hide-table-padding td {
-    padding: 0;
-  }
-
-  .expand-button {
-    position: relative;
-  }
-
-  .accordion-toggle .expand-button:after {
-    position: absolute;
-    left: .75rem;
-    top: 50%;
-    transform: translate(0, -50%);
-    content: '-';
-  }
-
-  .accordion-toggle.collapsed .expand-button:after {
-    content: '+';
-  }
-</style>
-<div class="container-fluid">
-  <h1 class="h1 title text-center mt-5">COVID-19 Overview</h1>
-  <div class="row mt-5">
-    <div class="row">
-      <div class="col-md-6" style="border-right: 1px solid grey ">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="row mb-3">
-              <div class="col-md-12">
-                <h1 class="title h1 text-center">India</h1>
-                <h3 class="h6 text-muted text-center">Last Update at {total.lastupdatedtime} </h3>
-              </div>
-            </div>
-          </div>
-          {#each overview as o}
-            <div class="col-md-3">
-              <div
-                class="text-center text-{ o.class }">
-                <h2 class="h1 mb-0">{ o.number }</h2>
-                <p class="h6 text-small">{#if o.delta}[+ {o.delta }]{/if}</p>
-                <p><strong>{ o.title }</strong></p>
-              </div>
-            </div>
-          {/each}
-        </div>
-      </div>
-
-      {#if global_update_date}
-        <div class="col-md-6">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="row mb-3">
-                <div class="col-md-12">
-                  <h1 class="title h1 text-center">Global</h1>
-                  <h3 class="h6 text-muted text-center">Last updated at {global_update_date}</h3>
-                </div>
-              </div>
-            </div>
-            {#each global_overview as o}
-              <div class="col-md-3">
-                <div class="text-center text-{ o.class }">
-                  <h2 class="h1 mb-0">{ o.number }</h2>
-                  <p class="h6 text-small">{#if o.delta}[+ {o.delta }]{/if}</p>
-                  <p><strong>{ o.title }</strong></p>
-                </div>
-              </div>
-            {/each}
-          </div>
-        </div>
-      {/if}
-    </div>
-    <div class="col-md-6 mt-3">
-      <h2 class="h2 text-center mb-3">State and District Wise</h2>
-      <div class="table-responsive">
-        <table class="table">
-          <thead class="black white-text">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">State</th>
-            <th scope="col">Confirmed</th>
-            <th scope="col">Active</th>
-            <th scope="col">Recovered</th>
-            <th scope="col">Deaths</th>
-          </tr>
-          </thead>
-          <tbody>
-
-          {#each statewise as state, i}
-            <tr class="accordion-toggle collapsed" id="accordion{i}" data-toggle="collapse" data-parent="#accordion{i}"
-                href="#collapse{i}">
-              <td class="expand-button"></td>
-              <td>{ state.state }</td>
-              <td>{ state.confirmed }</td>
-              <td>{ state.active }</td>
-              <td>{ state.recovered }</td>
-              <td>{ state.deaths }</td>
-            </tr>
-            <tr class="hide-table-padding table">
-              <td></td>
-              <td colspan="5">
-                <div id="collapse{i}" class="collapse in p-3">
-                  {#if state_data[state.state]}
-                    {#each state_data[state.state] as sd}
-                      <div class="row">
-                        <div class="col-4 m-2 text-center">{sd.district}</div>
-                        <div class="col-4 m-2 text-center">{sd.confirmed}</div>
-                      </div>
-                    {/each}
-                  {/if}
-                </div>
-              </td>
-            </tr>
-          {/each}
-          </tbody>
-        </table>
-      </div>
-
-    </div>
-{#if global_update_date}
-
-    <div class="col-md-6 mt-3">
-      <h2 class="h2 text-center mb-3">Top countries</h2>
-      <table class="table">
-        <thead class="black white-text">
-        <tr>
-          <th scope="col">Country</th>
-          <th scope="col">Confirmed</th>
-          <th scope="col">Recovered</th>
-          <th scope="col">Deaths</th>
-        </tr>
-        </thead>
-        <tbody>
-        {#each countries as c}
-        <tr>
-          <th scope="row">{c.Country}</th>
-          <td>{c.TotalConfirmed}</td>
-          <td>{c.TotalRecovered}</td>
-          <td>{c.TotalDeaths}</td>
-        </tr>
-        {/each}
-        </tbody>
-      </table>
-    </div>
-  {/if}
-  </div>
-</div>
 <script>
   let overview = []
+
+  import Overview from './Overview.svelte'
+  import Chart from './Chart.svelte'
   const axios = require('axios')
   let total = { lastupdatedtime: '-' }
   let statewise = []
@@ -198,10 +54,10 @@
       }
     ]
 
-    let state_res;
-    try{
+    let state_res
+    try {
       state_res = await axios.get('https://api.covid19india.org/state_district_wise.json')
-    }catch (e) {
+    } catch (e) {
       state_res = await axios.get('https://cors-anywhere.herokuapp.com/https://api.covid19india.org/state_district_wise.json')
     }
     let state_raw_data = state_res.data
@@ -216,14 +72,14 @@
 
     }
     state_data = state_data
-    let global;
+    let global
     try {
-      global = await axios.get('https://api.covid19api.com/summary');
+      global = await axios.get('https://api.covid19api.com/summary')
     } catch (e) {
-      global = await axios.get('https://cors-anywhere.herokuapp.com/https://api.covid19api.com/summary');
+      global = await axios.get('https://cors-anywhere.herokuapp.com/https://api.covid19api.com/summary')
     }
     let global_data = global.data.Global
-    countries = global.data.Countries.sort(sortByProperty('TotalConfirmed')).slice(0,37)
+    countries = global.data.Countries.sort(sortByProperty('TotalConfirmed')).slice(0, 37)
     global_overview = [
       {
         number: global_data.TotalConfirmed,
@@ -255,5 +111,119 @@
 
   })()
 
-
 </script>
+
+<style>
+  tr.hide-table-padding td {
+    padding: 0;
+  }
+
+  .expand-button {
+    position: relative;
+  }
+
+  .accordion-toggle .expand-button:after {
+    position: absolute;
+    left: .75rem;
+    top: 50%;
+    transform: translate(0, -50%);
+    content: '-';
+  }
+
+  .accordion-toggle.collapsed .expand-button:after {
+    content: '+';
+  }
+</style>
+<div class="container-fluid">
+  <h1 class="h1 title text-center mt-5">COVID-19 Overview</h1>
+  <div class="row mt-5">
+
+    <div class="row">
+      <div class="col-md-6" style="border-right: 1px solid grey ">
+        <Overview title="India" overview={overview} lastupdatedtime={total.lastupdatedtime}/>
+      </div>
+
+      {#if global_update_date}
+        <div class="col-md-6">
+          <Overview title="Global" overview={global_overview} lastupdatedtime={global_update_date}/>
+        </div>
+      {/if}
+    </div>
+    <div class="col-md-6 mt-3">
+      <h2 class="h2 text-center mb-3">State and District Wise</h2>
+      <div class="table-responsive">
+        <table class="table">
+          <thead class="black white-text">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">State</th>
+            <th scope="col">Confirmed</th>
+            <th scope="col">Active</th>
+            <th scope="col">Recovered</th>
+            <th scope="col">Deaths</th>
+          </tr>
+          </thead>
+          <tbody>
+
+          {#each statewise as state, i}
+            <tr class="accordion-toggle collapsed" id="accordion{i}" data-toggle="collapse" data-parent="#accordion{i}"
+                href="#collapse{i}">
+              <td class="expand-button"></td>
+              <td>{ state.state }</td>
+              <td>{ state.confirmed }</td>
+              <td>{ state.active }</td>
+              <td>{ state.recovered }</td>
+              <td>{ state.deaths }</td>
+            </tr>
+            <tr class="hide-table-padding table">
+              <td></td>
+              <td colspan="5">
+                <div id="collapse{i}" class="collapse in p-3">
+                  {#if state_data[state.state]}
+                    {#each state_data[state.state] as sd}
+                      <div class="row">
+                        <div class="col-4 m-2 text-center">{sd.district}</div>
+                        <div class="col-4 m-2 text-center">{sd.confirmed}</div>
+                      </div>
+                    {/each}
+                  {/if}
+                </div>
+              </td>
+            </tr>
+          {/each}
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+    {#if global_update_date}
+      <div class="col-md-6 mt-3">
+        <h2 class="h2 text-center mb-3">Top countries</h2>
+        <table class="table">
+          <thead class="black white-text">
+          <tr>
+            <th scope="col">Country</th>
+            <th scope="col">Confirmed</th>
+            <th scope="col">Recovered</th>
+            <th scope="col">Deaths</th>
+          </tr>
+          </thead>
+          <tbody>
+          {#each countries as c}
+            <tr>
+              <th scope="row">{c.Country}</th>
+              <td>{c.TotalConfirmed}</td>
+              <td>{c.TotalRecovered}</td>
+              <td>{c.TotalDeaths}</td>
+            </tr>
+          {/each}
+          </tbody>
+        </table>
+      </div>
+    {/if}
+    <div class="col-md-12">
+      <Chart />
+    </div>
+
+  </div>
+</div>
---
+++ src/views/Overview.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let overview;
+  export let lastupdatedtime;
+  export let title;
+</script>
+{#if overview}
+<div class="row">
+  <div class="col-md-12">
+    <div class="row mb-3">
+      <div class="col-md-12">
+        <h1 class="title h1 text-center">{title}</h1>
+        <h3 class="h6 text-muted text-center">Last Update at {lastupdatedtime} </h3>
+      </div>
+    </div>
+  </div>
+  {#each overview as o}
+    <div class="col-md-3">
+      <div
+        class="text-center text-{ o.class }">
+        <h2 class="h1 mb-0">{ o.number }</h2>
+        <p class="h6 text-small">{#if o.delta}[+ {o.delta }]{/if}</p>
+        <p><strong>{ o.title }</strong></p>
+      </div>
+    </div>
+  {/each}
+</div>
+{/if}
