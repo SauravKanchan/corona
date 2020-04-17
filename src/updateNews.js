671622cfda3c0a539746bd8b3c277233c9693f88
SauravKanchan <sauravnk30@gmail.com>
2020-04-17 16:58:22 +0530

    Add news json

--- src/updateNews.js
+++ src/updateNews.js
@@ -1,1 +1,13 @@
-console.log(process.env.API_KEY)
+require('dotenv').config()
+const axios = require('axios')
+const API_KEY = process.env.API_KEY
+const fs = require('fs');
+
+(async () => {
+  let res = await axios.get(`http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=4119ac59db8e4fc89b2118424f8f3363`)
+  if (res.status === 200) {
+    fs.writeFile('src/data/news.json', JSON.stringify(res.data), 'utf8', (e) => {
+      console.log(e)
+    })
+  }
+})()
