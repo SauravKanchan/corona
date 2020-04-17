e71f321d59bcfee4b3083696ead9bbca43130d51
SauravKanchan <sauravnk30@gmail.com>
2020-04-17 10:42:08 +0530

    Make previous button working

--- src/views/Information.svelte
+++ src/views/Information.svelte
@@ -6,7 +6,8 @@
   let contacts = []
   let title = 'What are you looking for?'
   let area1 = []
-  let step = 1;
+  let step = 1
+  let info_type_index;
 
   (async () => {
     info_data = await window.api('https://covid-fyi-backend-2.herokuapp.com/api/v1/covidfyi/info_types/')
@@ -18,37 +19,34 @@
     area1 = area1
   })()
 
-  async function update_contact (e) {
-    state = e.target.innerText
-    let contacts_temp = []
-    let res = await window.api(`https://covid-fyi-backend-2.herokuapp.com/api/v1/covidfyi/states/${state}/`)
-    for (let district in res.data) {
-      let district_data = res.data[district].entries
-      for (let entry in district_data) {
-        let temp_data = district_data[entry]
-        temp_data.district = res.data[district].district
-        contacts_temp.push(temp_data)
-      }
-    }
-    contacts = contacts_temp.filter((d) => {
-      return d.infotype == info_type
-    })
-    console.log(contacts)
-  }
-
   function update_data (e) {
     switch (step) {
+      case 0: {
+        area1 = []
+        for (let i in info_data) {
+          area1.push(info_data[i].info_type.name)
+        }
+        area1 = area1
+        title = 'What are you looking for?'
+        contacts = []
+        step = 1
+        break
+      }
       case 1: {
-        let data = e.target.getAttribute('data')
-        area1 = info_data[data].states
-        info_type = info_data[data].info_type.name
+        if (e) {
+          info_type_index = e.target.getAttribute('data')
+        }
+        area1 = info_data[info_type_index].states
+        info_type = info_data[info_type_index].info_type.name
         title = `${info_type}: Select your state`
         step = 2
         break
       }
       case 2: {
         (async () => {
-          state = e.target.getAttribute('value')
+          if (e) {
+            state = e.target.getAttribute('value')
+          }
           let contacts_temp = []
           let res = await window.api(`https://covid-fyi-backend-2.herokuapp.com/api/v1/covidfyi/states/${state}/`)
           for (let district in res.data) {
@@ -64,23 +62,46 @@
           })
           area1 = []
           title = `${info_type}: Contacts`
-          console.log(contacts)
+          step = 3
         })()
       }
     }
   }
 
+  function previous () {
+    step -= 2
+    update_data()
+    // console.log(step)
+    // switch (step) {
+    //   case 1:{
+    //     area1 = []
+    //     for (let i in info_data) {
+    //       area1.push(info_data[i].info_type.name)
+    //     }
+    //     area1 = area1
+    //     title = "What are you looking for?"
+    //     contacts = []
+    //     break;
+    //   }
+    //   case 2:{
+    //     console.log(area1)
+    //     area1 = area1
+    //     contacts = []
+    //     break;
+    //   }
+    // }
+  }
+
 </script>
 <div class="container-fluid mt-5">
   <div class="row">
-    <div class="col-md-2">
-      <button class="btn btn-primary w-100"><i class="fas fa-arrow-left"></i>  Previous</button>
-    </div>
-    <div class="col-md-8">
-      <h1 class="h1 title text-center">{title}</h1>
+    <div class="col-md-10">
+      <h1 class="h1 title">{title}</h1>
     </div>
     <div class="col-md-2">
-      <button class="btn btn-primary w-100">Next  <i class="fas fa-arrow-right"></i></button>
+      <button class="btn btn-primary w-100" disabled='{step!==2 && step!==3}' on:click={previous}><i
+        class="fas fa-arrow-left"></i> Previous
+      </button>
     </div>
   </div>
   <div class="row mt-3" id="area1">
@@ -92,7 +113,7 @@
     {/each}
   </div>
   <div class="row" id="area2">
-      {#each contacts as ele, i}
+    {#each contacts as ele, i}
       <div class="col-sm-3 mb-5">
         <div class="card" style="width: 18rem;">
           <div class="card-body justify-content-center">
@@ -103,6 +124,6 @@
           </div>
         </div>
       </div>
-      {/each}
+    {/each}
   </div>
 </div>
