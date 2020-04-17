83fd103dc264b302a1a9689d123bfc4be3129dd9
SauravKanchan <sauravnk30@gmail.com>
2020-04-18 00:50:17 +0530

    Update News

--- src/updateNews.js
+++ src/updateNews.js
@@ -14,7 +14,7 @@ async function gitPush () {
 }
 
 (async () => {
-  let res = await axios.get(`http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=4119ac59db8e4fc89b2118424f8f3363`)
+  let res = await axios.get(`http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=${API_KEY}`)
   if (res.status === 200) {
     fs.writeFile('src/data/news.json', JSON.stringify(res.data), 'utf8', async (e) => {
       if (!e) {
