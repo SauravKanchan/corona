811ec173b48988cc01498d57ca3a33bcfd2e4c77
SauravKanchan <sauravnk30@gmail.com>
2020-04-23 16:15:17 +0530

    Fix card columns in desktop view

--- src/views/News.svelte
+++ src/views/News.svelte
@@ -1,43 +1,35 @@
 <script>
-  export let title;
-  title = "News";
+  export let title
+  title = 'News'
   let news;
   (async () => {
-    let res;
-    res = await api.get('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json');
+    let res
+    res = await api.get('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json')
     news = res.data.articles
   })()
 </script>
 
 <div class="container-fluid mt-5">
   {#if news}
-  <div class="row">
-    {#each news as n}
-      <div class="col-md-3 mb-5">
-        <div class="card card-cascade">
-          <!-- Card image -->
+    <div class="card-columns">
+      {#each news as n}
+        <div class="card">
           <div class="view view-cascade overlay">
             <img class="card-img-top" src="{n.urlToImage}" alt="Card image cap">
             <a>
               <div class="mask rgba-white-slight"></div>
             </a>
           </div>
-          <!-- Card content -->
           <div class="card-body card-body-cascade text-center">
-            <!-- Title -->
             <h4 class="card-title"><strong>{n.title}</strong></h4>
-            <!-- Subtitle -->
             {#if n.author}<h6 class="font-weight-bold indigo-text py-2">{n.author}</h6>{/if}
             <!-- Text -->
             <p class="card-text">{n.description}</p>
-
             <a class="btn btn-unique" href="{n.url}" target="_blank">Read More</a>
           </div>
-
         </div>
-      </div>
-    {/each}
-  </div>
+      {/each}
+    </div>
 
   {:else}
     <h1 class="h1 title center">News not loaded</h1>
